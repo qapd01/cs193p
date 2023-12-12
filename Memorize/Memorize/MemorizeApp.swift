2fa16202dbeeeeec3d6ca03a7bfd3078d3e3605c
boss <bossxlp@gmail.com>
2023-12-12 21:29:45 +0700

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by M.Sorawid on 12/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
