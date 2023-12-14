7267a8b26e0d6ed0e6ab4808802d543f603c08aa
boss <bossxlp@gmail.com>
2023-12-14 09:17:42 +0700

    feat: end of session 3

---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by M.Sorawid on 14/12/2566 BE.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    var model: MemoryGame<String>
+    
+}
