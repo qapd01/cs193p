02b354565f97bb245b59f8c593e322c983cefc72
boss <bossxlp@gmail.com>
2024-01-06 19:06:43 +0700

    feat: end of session 6

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -7,7 +7,37 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    var model: MemoryGame<String>
+class EmojiMemoryGame: ObservableObject {
+    
+    private static let emojis = ["👻","🎃","🕷️","😈","💀","🕸️","🧙‍♂️","🙀","👹","😱","☠️","🍭"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return MemoryGame(
+            numberOfPairsOfCards: 10) { pairIndex in
+                if emojis.indices.contains(pairIndex){
+                    return emojis[pairIndex]
+                } else {
+                    return "‼️"
+                }
+                
+            }
+    }
+    
+    
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle(){
+        model.shuffle()
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
     
 }
---
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,69 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by M.Sorawid on 12/12/2566 BE.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    private let aspectRatio : CGFloat = 2/3
+        
+    var body: some View {
+        VStack{
+            
+                cards
+                    .animation(.default, value: viewModel.cards)
+            
+            Button("shuffle"){
+                viewModel.shuffle()
+            }
+        }
+        .padding()
+    }
+    
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
+        }
+        .foregroundColor(.orange)
+    }
+    
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+                .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+#Preview {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+}
